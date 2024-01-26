98ed21e1fcdd3296c46b1c3870c997d3f89e1f9f
Conor Doyle <18cldoyle@stbenilduscollege.com>
2024-01-26 09:53:28 +0000

    finished basic

--- Artefact/dataProcessing.py
+++ Artefact/dataProcessing.py
@@ -5,6 +5,10 @@ soundlevel = []
 time=[]                 # estabhlising lists 
 tempreture = []
 waterTakenIn = []
+tmpSound = []
+tmpTime=[]
+tmpTemp=[]
+TmpWater=[]
 
 file=open("tmp.csv","r")
 dataIn=file.read()
@@ -17,14 +21,40 @@ print(tmpList)
 
 for i in tmpList: # goes through temp data list and figures out what final list to assign them to 
     if "S" in i: 
-        soundlevel.append(i)
+        tmpSound.append(i)
     elif "T" in i: 
-        time.append(i)
+        tmpTime.append(i)
     elif "C" in i: 
-        tempreture.append(i)
+        tmpTemp.append(i)
     elif "A" in i:
-        waterTakenIn.append(i)
-    
+        TmpWater.append(i)
+
+
+for i in tmpSound:
+    tmpSound.remove(i)
+    i=i[:-1]
+    soundlevel.append(i)
+    if i == "":
+        soundlevel.remove(i) # valitdating  again 
+for i in tmpTime:
+    tmpTime.remove(i)
+    i=i[:-1]
+    time.append(i)
+    if i =="":
+        time.remove(i)
+for i in tmpTemp:
+    tmpTemp.remove(i)
+    i=i[:-1]
+    tempreture.append(i)
+    if i=="":
+        tempreture.remove(i)
+for i in TmpWater:
+    TmpWater.remove(i)
+    i=i[:-1]
+    waterTakenIn.append(i)
+    if i== "":
+        waterTakenIn.remove(i)
+
 print(soundlevel)
 print(time)
 print(tempreture)
