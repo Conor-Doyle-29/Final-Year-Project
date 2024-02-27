87ea3e78b4496f048f990fe61d3c448c2f6eede9
Conor Doyle <18cldoyle@stbenilduscollege.com>
2024-02-27 10:50:52 +0000

    created a list genrator and fixed it

--- Artefact/dataProcessing.py
+++ Artefact/dataProcessing.py
@@ -1,7 +1,9 @@
 import statistics as stat
 import matplotlib.pyplot as plt         #importing whats required 
 import collections 
+import random
 from studyScore import studyscore as study
+from listGenerator import listMaker as maker
 soundlevel = []
 time=[]                 # estabhlising lists 
 tempreture = []
@@ -19,48 +21,49 @@ print(dataIn)
 tmpList= dataIn.split(",")
 tmpList.remove(tmpList[-1])
 print(tmpList)
-
-for i in tmpList: # goes through temp data list and figures out what final list to assign them to 
-    if "S" in i: 
-        tmpSound.append(i)
-    elif "T" in i: 
-        tmpTime.append(i)
-    elif "C" in i: 
-        tmpTemp.append(i)
-    elif "A" in i:
-        TmpWater.append(i)
-
-
-for i in tmpSound:
-    tmpSound.remove(i)
-    i=i[:-1]
-    i=int(i)
-    soundlevel.append(i)
-    if i == "":
-        soundlevel.remove(i) # valitdating  again 
-for i in tmpTime:
-    tmpTime.remove(i)
-    i=i[:-1]
-    i=int(i)
-    time.append(i)
-    if i =="":
-        time.remove(i)
-for i in tmpTemp:
-    tmpTemp.remove(i)
-    i=i[:-1]
-    i=int(i)
-    tempreture.append(i)
-    if i=="":
-        tempreture.remove(i)
-for i in TmpWater:
-    TmpWater.remove(i)
-    i=i[:-1]
-    i=int(i)
-    waterTakenIn.append(i)
-    if i== "":
-        waterTakenIn.remove(i)
+def dataProcess(dataset):
+    for i in dataset: # goes through temp data list and figures out what final list to assign them to 
+        if "S" in i: 
+            tmpSound.append(i)
+        elif "T" in i: 
+            tmpTime.append(i)
+        elif "C" in i: 
+            tmpTemp.append(i)
+        elif "A" in i:
+            TmpWater.append(i)
 
 
+    for i in tmpSound:
+        tmpSound.remove(i)
+        i=i[:-1]
+        i=int(i)
+        soundlevel.append(i)
+        if i == "":
+            soundlevel.remove(i) # valitdating  again 
+    for i in tmpTime:
+        tmpTime.remove(i)
+        i=i[:-1]
+        i=int(i)
+        time.append(i)
+        if i =="":
+            time.remove(i)
+    for i in tmpTemp:
+        tmpTemp.remove(i)
+        i=i[:-1]
+        i=int(i)
+        tempreture.append(i)
+        if i=="":
+            tempreture.remove(i)
+    for i in TmpWater:
+        TmpWater.remove(i)
+        i=i[:-1]
+        i=int(i)
+        waterTakenIn.append(i)
+        if i== "":
+            waterTakenIn.remove(i)
+
+processedList= dataProcess(tmpList)
+print(processedList)
 soundlevelmean= stat.mean(soundlevel)
 print(soundlevelmean)
 timeMean= stat.mean(time)
@@ -88,3 +91,7 @@ print(waterTakenInMean)
 # Avanced requirments 
 studyscores=study(soundlevelmean,time,tempretureMean,waterTakenInMean)
 print(studyscores)
+
+#What if 1: What if the noise level is to high
+dataSet1=maker()
+print(dataSet1)
---
+++ Artefact/listGenerator.py
@@ -0,0 +1,14 @@
+import random
+def listMaker():
+
+    dataset=[]
+    for i in range(0,1000):
+        tempreture= str((random.randint(10,30))) + "C"
+        dataset.append(tempreture)
+        water = str((random.randint(950,3000)) )+ "A"
+        dataset.append(water)
+        sound = str((random.randint(0,50))) + "S"
+        dataset.append(sound)
+    return dataset
+
+
