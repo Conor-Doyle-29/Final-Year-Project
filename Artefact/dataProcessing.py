3ca9033a525382351cc63f5e6b4a0b462c85ec8a
Conor Doyle <18cldoyle@stbenilduscollege.com>
2024-03-06 20:56:51 +0000

    just need to graph

--- Artefact/dataProcessing.py
+++ Artefact/dataProcessing.py
@@ -1,5 +1,5 @@
 import statistics as stat
-#import matplotlib.pyplot as plt         #importing whats required 
+import matplotlib.pyplot as plt         #importing whats required 
 import collections 
 import random
 from studyScore import studyscore as study
@@ -25,52 +25,9 @@ print(dataIn)
 tmpList= dataIn.split(",")
 tmpList.remove(tmpList[-1])
 print(tmpList)
-'''''
-def dataProcess(dataset):
-    for i in dataset: # goes through temp data list and figures out what final list to assign them to 
-        if "S" in i: 
-            tmpSound.append(i)
-        elif "T" in i: 
-            tmpTime.append(i)
-        elif "C" in i: 
-            tmpTemp.append(i)
-        elif "A" in i:
-            TmpWater.append(i)
 
-
-    for i in tmpSound:
-        tmpSound.remove(i)
-        i=i[:-1]
-        i=int(i)
-        soundlevel.append(i)
-        if i == "":
-            soundlevel.remove(i) # valitdating  again 
-    for i in tmpTime:
-        tmpTime.remove(i)
-        i=i[:-1]
-        i=int(i)
-        time.append(i)
-        if i =="":
-            time.remove(i)
-    for i in tmpTemp:
-        tmpTemp.remove(i)
-        i=i[:-1]
-        i=int(i)
-        tempreture.append(i)
-        if i=="":
-            tempreture.remove(i)
-    for i in TmpWater:
-        TmpWater.remove(i)
-        i=i[:-1]
-        i=int(i)
-        waterTakenIn.append(i)
-        if i== "":
-            waterTakenIn.remove(i)
-    return waterTakenIn,tempreture,time, soundlevel
-'''
 waterTakenIn,tempreture,time,soundlevel= process(tmpList)
 
-print(processedList)
 soundlevelmean= stat.mean(soundlevel)
 print(soundlevelmean)
 timeMean= stat.mean(time)
@@ -104,14 +61,15 @@ print(waterTakenInMean)
 
 
 
-# Avanced requirments 
 studyscores=study(soundlevelmean,time,tempretureMean,waterTakenInMean)
 print(studyscores)
+# Avanced requirments 
+
 
 #What if 1: What if the noise level is to high
 dataSet1=noise()
 #print(dataSet1)
-process(dataSet1)
+waterTakenIn,tempreture,time,soundlevel= process(dataSet1)
 
 soundlevelmean= stat.mean(soundlevel)
 print(soundlevelmean)
@@ -119,6 +77,8 @@ tempretureMean= stat.mean(tempreture)
 print(tempretureMean)
 waterTakenInMean= stat.mean(waterTakenIn)
 print(waterTakenInMean)
+
+
 studyscores=study(soundlevelmean,time,tempretureMean,waterTakenInMean)
 print(studyscores)
 
@@ -126,15 +86,15 @@ print(studyscores)
 # What If question 2 what if the tempreture is to high 
 dataSet2 = temp()
 print(dataSet2)
-tmpSound=[]
-tmpTime=[]
-tmpTemp=[]
-TmpWater=[]
-soundlevel = []
-time=[]                 # estabhlising lists 
-tempreture = []
-waterTakenIn = []
+waterTakenIn,tempreture,time,soundlevel= process(dataSet2)
 
+soundlevelmean= stat.mean(soundlevel)
+print(soundlevelmean)
+tempretureMean= stat.mean(tempreture)
+print(tempretureMean)
+waterTakenInMean= stat.mean(waterTakenIn)
+print(waterTakenInMean)
 
+studyscores=study(soundlevelmean,time,tempretureMean,waterTakenInMean)
 
 
